7685a5fa8560b2d21f9189a13e1c812906d48b24
Fahamida-Asha024 <168821114+Fahamida-Asha024@users.noreply.github.com>
2025-05-17 16:59:13 +0600

    Creat a new game Brick breaker using pong game concept

---
+++ src/Breakbrick/Main.java
@@ -0,0 +1,19 @@
+package Breakbrick;
+import javax.swing.JFrame;
+
+public class Main {
+
+ public static void main(String[] args) {
+
+  JFrame obj = new JFrame();
+  Gameplay gameplay = new Gameplay();
+  obj.setBounds(10, 10, 700, 600);
+  obj.setLocationRelativeTo(null);
+  obj.setTitle("Brick Breaker!");
+  obj.setResizable(false);
+  obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  obj.add(gameplay); // Add before making it visible
+  obj.setVisible(true);
+     gameplay.requestFocusInWindow();
+ }
+}
